40363435795ee587927fd3b0098fa0b65920359f
Tanner Rogalsky <tanner@tannerrogalsky.com>
2021-01-04 19:12:11 -0500

    production websocket endpoint

--- src/index.js
+++ src/index.js
@@ -6,60 +6,8 @@ const reactAnchor = document.createElement('div');
 document.body.appendChild(reactAnchor);
 render(reactAnchor);
 
-store.dispatch(wsConnect({ host: 'ws://localhost:8080/connect' }));
-
-async function run() {
-	// const signalingChannel = await new Promise(function(resolve, reject) {
-	// 	let connection = new WebSocket('ws://localhost:8080/connect');
-	// 	let onOpen = (event) => {
-	// 		connection.removeEventListener('open', onOpen);
-	// 		connection.removeEventListener('error', onError);
-	// 		resolve(connection);
-	// 	};
-	// 	let onError = (event) => {
-	// 		connection.removeEventListener('open', onOpen);
-	// 		connection.removeEventListener('error', onError);
-	// 		reject(event);
-	// 	};
-	// 	connection.addEventListener('open', onOpen);
-	// 	connection.addEventListener('error', onError);
-	// 	connection.addEventListener('error', (event) => {
-	// 		console.error('SOCKET ERROR', event);
-	// 	});
-
-	// 	connection.addEventListener('close', (event) => {
-	// 		console.error('SOCKET CLOSE', event);
-	// 	});
-	// });
-
-	// let peerConnection = null;
-	// signalingChannel.addEventListener('message', async (event) => {
-	// 	console.log('SOCKET MSG', event.data);
-	// 	const msg = JSON.parse(event.data);
-
-	// 	if (msg.simple_peer_signal) {
-	// 		peerConnection.signal(msg.simple_peer_signal);
-	// 	} else if (msg.AllRooms) {
-	// 		store.dispatch(setRooms(msg.AllRooms.rooms));
-	// 	} else if (typeof(msg) == "number") {
-	// 		store.dispatch(setUserId(msg));
-
-	// 		const isHost = msg % 2 == 0;
-	// 		peerConnection = new Peer({ initiator: isHost });
-	// 		console.log("IS HOST", isHost);
-	// 		peerConnection.on('signal', data => {
-	// 			signalingChannel.send(JSON.stringify({simple_peer_signal: data}));
-	// 		});
-	// 		peerConnection.on('connect', () => {
-	// 			console.log("connected");
-	// 			let payload = isHost ? "from host" : "from client";
-	// 			peerConnection.send(payload);
-	// 		});
-	// 		peerConnection.on('data', data => {
-	// 			console.log('data', new TextDecoder("utf-8").decode(data));
-	// 		});
-	// 	}
-	// });
+if (process.env.NODE_ENV !== 'production') {
+	store.dispatch(wsConnect({ host: 'wss://salty-mesa-40253.herokuapp.com/connect' }));
+} else { 
+	store.dispatch(wsConnect({ host: 'ws://localhost:8080/connect' }));
 }
-
-run();
