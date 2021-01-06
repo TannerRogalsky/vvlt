c7986234b6ef1f0135c2ba59b7f7aeeb6f23c52e
Tanner Rogalsky <tanner@tannerrogalsky.com>
2021-01-06 07:18:49 -0500

    move game state into middleware

--- src/stores/peer.js
+++ src/stores/peer.js
@@ -1,6 +1,16 @@
 import { createAction } from '@reduxjs/toolkit';
 import Peer from 'simple-peer';
 import { wsSend } from './websocket';
+import { Renderer, RendererSettings, VvltClient } from 'vvlt';
+
+import player1ImageSrc from '../../../../../rust/vult/resources/images/playerShip1_red.png';
+import enemy1ImageSrc from '../../../../../rust/vult/resources/images/enemyBlack1.png';
+
+const player1Image = new Image();
+player1Image.src = player1ImageSrc;
+
+const enemy1Image = new Image();
+enemy1Image.src = enemy1ImageSrc;
 
 export const p2pConnect = createAction('P2P_CONNECT', function prepare(any) {
   return {
@@ -19,9 +29,17 @@ export const p2pSend = createAction('P2P_NEW_MESSAGE', function prepare(any) {
 export const p2pSignal = createAction('P2P_SIGNAL', function prepare(any) {
   return any;
 });
+export const newCanvas = createAction('NEW_GAME_CANVAS', function prepare(any) {
+  return {
+    payload: any
+  };
+});
 
 const peerMiddleware = () => {
   let peerConnection = null;
+  let vvlt = null;
+  let isHost = null;
+  let raf = null;
 
   const onConnect = store => () => {
     console.log("p2p", "onConnect");
@@ -46,8 +64,8 @@ const peerMiddleware = () => {
     store.dispatch(wsSend('LeaveRoom'));
   }
 
-  const onData = store => (event) => {
-    console.log('p2pData', event);
+  const onData = store => (data) => {
+    vvlt.handle_remote_input(data);
   }
 
   // the middleware part of this function
@@ -61,6 +79,7 @@ const peerMiddleware = () => {
         console.log('P2P_CONNECT', action.payload);
         // connect to the remote host
         peerConnection = new Peer({ initiator: action.payload });
+        isHost = action.payload;
 
         peerConnection.on('signal', onSignal(store));
         peerConnection.on('connect', onConnect(store));
@@ -76,12 +95,79 @@ const peerMiddleware = () => {
         // console.log('websocket closed');
         break;
       case 'P2P_NEW_MESSAGE':
-        console.log('sending a message', action);
         peerConnection.send(action.payload);
         break;
       case 'P2P_SIGNAL':
-        console.log(action);
         peerConnection.signal(JSON.parse(action.payload))
+        break;
+      case 'NEW_GAME_CANVAS':
+        let canvas = action.payload;
+        if (canvas === null) {
+          cancelAnimationFrame(raf);
+          break;
+        }
+
+        const settings = new RendererSettings(canvas, player1Image, enemy1Image);
+        const renderer = new Renderer(settings);
+        renderer.set_width_height(512, 512);
+        vvlt = new VvltClient(isHost);
+
+        document.addEventListener('keydown', (event) => {
+          event.preventDefault();
+          let input = null;
+          switch (event.key) {
+            case 'ArrowUp':
+              input = vvlt.handle_up_pressed();
+              break;
+            case 'ArrowDown':
+              input = vvlt.handle_down_pressed();
+              break;
+            case 'ArrowLeft':
+              input = vvlt.handle_left_pressed();
+              break;
+            case 'ArrowRight':
+              input = vvlt.handle_right_pressed();
+              break;
+            case ' ':
+              input = vvlt.handle_fire_pressed();
+              break;
+          }
+          if (input) {
+            store.dispatch(p2pSend(input))
+          }
+        });
+
+        document.addEventListener('keyup', (event) => {
+          event.preventDefault();
+          let input = null;
+          switch (event.key) {
+            case 'ArrowUp':
+              input = vvlt.handle_up_released();
+              break;
+            case 'ArrowDown':
+              input = vvlt.handle_down_released();
+              break;
+            case 'ArrowLeft':
+              input = vvlt.handle_left_released();
+              break;
+            case 'ArrowRight':
+              input = vvlt.handle_right_released();
+              break;
+            case ' ':
+              input = vvlt.handle_fire_released();
+              break;
+          }
+          if (input) {
+            store.dispatch(p2pSend(input))
+          }
+        });
+
+        const loop = (time) => {
+          vvlt.step();
+          vvlt.render(renderer);
+          raf = requestAnimationFrame(loop);
+        }
+        raf = requestAnimationFrame(loop);
         break;
       default:
         // console.log('the next action:', action);
--- src/views/game.tsx
+++ src/views/game.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Renderer, RendererSettings, VvltClient } from 'vvlt';
-
-import store, { State, Room } from '../store';
-import { p2pSend } from '../stores/peer';
-
-import player1ImageSrc from '../../../../../rust/vult/resources/images/playerShip1_red.png';
-import enemy1ImageSrc from '../../../../../rust/vult/resources/images/enemyBlack1.png';
-
-const player1Image = new Image();
-player1Image.src = player1ImageSrc;
-
-const enemy1Image = new Image();
-enemy1Image.src = enemy1ImageSrc;
+import { connect, useDispatch } from 'react-redux';
+// @ts-ignore
+import { newCanvas } from '../stores/peer';
+import { State, Room, } from '../store';
 
 interface PassedProps {
 	room: Room,
@@ -33,73 +22,9 @@ export default connect(
 	null,
 )(function Game({ room, user_id }: GameProps) {
 
+	const dispatch = useDispatch();
 	const refCallback = (canvas: HTMLCanvasElement) => {
-		if (canvas === null) {
-			// TODO: cleanup?
-			return;
-		}
-		
-		const settings = new RendererSettings(canvas, player1Image, enemy1Image);
-		const renderer = new Renderer(settings);
-		renderer.set_width_height(512, 512);
-		const vvlt = new VvltClient(room.host.id === user_id);
-
-		document.addEventListener('keydown', (event) => {
-			event.preventDefault();
-			let input = null;
-			switch (event.key) {
-				case 'ArrowUp':
-					input = vvlt.handle_up_pressed();
-					break;
-				case 'ArrowDown':
-					input = vvlt.handle_down_pressed();
-					break;
-				case 'ArrowLeft':
-					input = vvlt.handle_left_pressed();
-					break;
-				case 'ArrowRight':
-					input = vvlt.handle_right_pressed();
-					break;
-				case ' ':
-					input = vvlt.handle_fire_pressed();
-					break;
-			}
-			if (input) {
-				store.dispatch(p2pSend(input))
-			}
-		});
-
-		document.addEventListener('keyup', (event) => {
-			event.preventDefault();
-			let input = null;
-			switch (event.key) {
-				case 'ArrowUp':
-					input = vvlt.handle_up_released();
-					break;
-				case 'ArrowDown':
-					input = vvlt.handle_down_released();
-					break;
-				case 'ArrowLeft':
-					input = vvlt.handle_left_released();
-					break;
-				case 'ArrowRight':
-					input = vvlt.handle_right_released();
-					break;
-				case ' ':
-					input = vvlt.handle_fire_released();
-					break;
-			}
-			if (input) {
-				store.dispatch(p2pSend(input))
-			}
-		});
-
-		const loop = (time: DOMHighResTimeStamp) => {
-			vvlt.step();
-			vvlt.render(renderer);
-			requestAnimationFrame(loop);
-		}
-		requestAnimationFrame(loop);
+		dispatch(newCanvas(canvas));
 	}
 
 	return (
